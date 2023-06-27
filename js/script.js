a3556495b05e0f8f291971ac0032e90bd863dff6
Maria Eduarda <105657790+mariaassuncao26@users.noreply.github.com>
2023-06-27 10:25:49 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,28 +2,28 @@ posicao1 = 0;
 posicao2 = 0;
 
 $("#btnDireita").click(() => {
-        posicao1 += 10
+        posicao1 += 30
         $("#anima-div").animate({left: posicao1 + "px"}, {duration: 300})
         $("#anima-div").removeClass("anima-div-esquerda anima-div-cima anima-div-baixo").addClass("anima-div")
     }
 )
 
 $("#btnEsquerda").click(() => {
-        posicao1 -= 10
+        posicao1 -= 30
         $("#anima-div").animate({left: posicao1 + "px"}, {duration: 300})
         $("#anima-div").removeClass("anima-div anima-div-cima anima-div-baixo").addClass("anima-div-esquerda")
     }
 )
 
 $("#btnCima").click(() => {
-        posicao2 -= 10
+        posicao2 -= 30
         $("#anima-div").animate({top: posicao2 + "px"}, {duration: 300})
         $("#anima-div").removeClass("anima-div anima-div-esquerda anima-div-baixo").addClass("anima-div-cima")
     }
 )
 
 $("#btnBaixo").click(() =>{ 
-        posicao2 += 10
+        posicao2 += 30
         $("#anima-div").animate({top: posicao2 + "px"}, {duration: 300})
         $("#anima-div").removeClass("anima-div anima-div-esquerda anima-div-cima").addClass("anima-div-baixo")
     }
