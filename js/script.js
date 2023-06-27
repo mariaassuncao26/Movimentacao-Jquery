897a41243cb36604b484857ea4b46bb3db7979ab
Maria Eduarda <105657790+mariaassuncao26@users.noreply.github.com>
2023-06-27 07:29:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,28 +4,28 @@ posicao2 = 0;
 $("#btnDireita").click(() => {
         posicao1 += 10
         $("#anima-div").animate({left: posicao1 + "px"}, {duration: 300})
-        $("#anima-div").removeclass("anima-div-esquerda anima-div-cima anima-div-baixo").addClass("anima-div")
+        $("#anima-div").removeClass("anima-div-esquerda anima-div-cima anima-div-baixo").addClass("anima-div")
     }
 )
 
 $("#btnEsquerda").click(() => {
         posicao1 -= 10
         $("#anima-div").animate({left: posicao1 + "px"}, {duration: 300})
-        $("#anima-div").removeclass("anima-div anima-div-cima anima-div-baixo").addClass("anima-div-esquerda")
+        $("#anima-div").removeClass("anima-div anima-div-cima anima-div-baixo").addClass("anima-div-esquerda")
     }
 )
 
 $("#btnCima").click(() => {
         posicao2 -= 10
         $("#anima-div").animate({top: posicao2 + "px"}, {duration: 300})
-        $("#anima-div").removeclass("anima-div anima-div-esquerda anima-div-baixo").addClass("anima-div-cima")
+        $("#anima-div").removeClass("anima-div anima-div-esquerda anima-div-baixo").addClass("anima-div-cima")
     }
 )
 
 $("#btnBaixo").click(() =>{ 
         posicao2 += 10
         $("#anima-div").animate({top: posicao2 + "px"}, {duration: 300})
-        $("#anima-div").removeclass("anima-div anima-div-esquerda anima-div-cima").addClass("anima-div-baixo")
+        $("#anima-div").removeClass("anima-div anima-div-esquerda anima-div-cima").addClass("anima-div-baixo")
     }
 )
 
